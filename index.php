68a52627b64a4d91f94f0009ca03aa83a8364373
Andrey <AndreyWebDays@gmail.com>
2022-03-18 16:01:53 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+function calculatePolishNotation($expression)
+{
+    $new_array = explode(" ", $expression);
+    
+    
+    
+    return $new_array;
+}
+
+var_dump (calculatePolishNotation('5 1 2 + 4 * + 3 -'));
